df7d347f09620477d4e86777e54f2c1f3911ad76
Imre Maczkó <immaczko@gmail.com>
2018-11-07 13:28:00 +0100

    commit for pull

--- src/com/codecool/klondike/Game.java
+++ src/com/codecool/klondike/Game.java
@@ -80,8 +80,12 @@ public class Game extends Pane {
         Card card = (Card) e.getSource();
         Pile pile = getValidIntersectingPile(card, tableauPiles);
         //TODO
-        if (pile != null) {
+        Pile foundationPile = getValidIntersectingPile(card, foundationPiles);
+
+        if ((pile) != null) {
             handleValidMove(card, pile);
+        } else if (foundationPile != null) {
+            handleValidMove(card, foundationPile);
         } else {
             draggedCards.forEach(MouseUtil::slideBack);
             draggedCards = null;
@@ -211,23 +215,6 @@ public class Game extends Pane {
                 BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
     }
 
-    private void handle(MouseEvent e) {
-        if (draggedCards.isEmpty())
-            return;
-        Card card = (Card) e.getSource();
-        Pile pile = getValidIntersectingPile(card, tableauPiles);
-        //TODO
-        Pile foundationPile = getValidIntersectingPile(card, foundationPiles);
-
-        if ((pile) != null) {
-            handleValidMove(card, pile);
-        } else if (foundationPile != null) {
-            handleValidMove(card, foundationPile);
-        } else {
-            draggedCards.forEach(MouseUtil::slideBack);
-            draggedCards = null;
-        }
-    }
     public boolean isUnderCard(Card card, Pile pile) {
         Rank cardNumber = card.getRank();
         int cardNumberInInt = cardNumber.getRankNumber();
